c7c5976553767433999d1b6722228086dc69aa83
root <147520338+root-akina@users.noreply.github.com>
2023-11-09 10:52:11 +0800

    Rabbit-MQ-发送消息

---
+++ publisher/src/test/java/testMQ.java
@@ -0,0 +1,22 @@
+import com.itheima.publisher.PublisherApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest(classes = PublisherApplication.class)
+public class testMQ {
+
+    @Autowired
+    private RabbitTemplate rabbit;
+
+    @Test
+    public void simpleQueue(){
+
+        String queue = "simple.queue";
+        String msg = "hello"+ LocalDateTime.now();
+        rabbit.convertAndSend(queue,msg);
+    }
+}
