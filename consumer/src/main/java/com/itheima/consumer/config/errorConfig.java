e34ec8f0d0d3779c61dd08c7c876e7fb71ce0b45
root <147520338+root-akina@users.noreply.github.com>
2023-11-11 16:54:56 +0800

    Redis-Advanced

--- consumer/src/main/java/com/itheima/consumer/config/errorConfig.java
+++ consumer/src/main/java/com/itheima/consumer/config/errorConfig.java
@@ -2,7 +2,9 @@ package com.itheima.consumer.config;
 
 public class errorConfig {
     public static void main(String[] args) {
+        System.out.println("day_11/11_01");
         System.out.println("day_11/11_02");
+        System.out.println("day_11/11_03");
 
     }
 }
