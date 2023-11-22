04a8949a001deec6f5d9747f161f457be2b4f665
root <147520338+root-akina@users.noreply.github.com>
2023-11-22 22:52:25 +0800

    commit

--- consumer/src/main/java/com/itheima/consumer/config/errorConfig.java
+++ consumer/src/main/java/com/itheima/consumer/config/errorConfig.java
@@ -6,6 +6,8 @@ public class errorConfig {
         System.out.println("day_11/11_02");
         System.out.println("day_11/11_03");
         System.out.println();
+        System.out.println();
+        System.out.println();
 
     }
 }
